bec45bf53eb411d0214464aa610101e1fc916acd
angeldavidm6a <angeldavvid41@gmail.com>
2020-10-30 20:14:31 -0500

    Segunda subida

--- src/main/java/com/prueba/tecnica/latan/ui/PageTarget.java
+++ src/main/java/com/prueba/tecnica/latan/ui/PageTarget.java
@@ -3,7 +3,7 @@ package com.prueba.tecnica.latan.ui;
 import net.serenitybdd.screenplay.targets.Target;
 
 public class PageTarget {
-
+    //coment
     public static final Target MIS_VIAJES=Target.the("Mis viajes").locatedBy("(//p[contains(text(),'Mis viajes')])[1]");
     public static final Target DONDE_LO_ENCUENTRAS=Target.the("Donde lo encuentras").locatedBy("//a[@role='button']");
     public static final Target CODIGO_RESERVA=Target.the("Codigo reserva").locatedBy("//span[@class='record-locator']");
